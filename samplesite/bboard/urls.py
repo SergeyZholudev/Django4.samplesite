3d97e363c4390afc3927ed3aab750869e16b3e9b
Sergey Zholudev <serg.zholudev@gmail.com>
2024-08-19 16:46:10 +0300

    Views ans URL's have been changed. message by pk was added. index was removed

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('<int:rubric_id>/', views.rubric_bbs),
+    # path('', views.index),
 ]
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from .models import Bb
+from .models import Bb, Rubric
 
 
 # Create your views here.
 
-def index(request):
-    bbs = Bb.objects.all()
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+# def index(request):
+#     bbs = Bb.objects.all()
+#     return render(request, 'bboard/index.html', {'bbs': bbs})
+
+
+def rubric_bbs(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics,
+               'current_rubric': current_rubric}
+    return render(request, 'bboard/rubric_bbs.html', context)
